a1a381f7671b077df0175931b7a274e784fa2000
huye123 <125990959@qq.com>
2019-04-24 17:20:48 +0800

    huye交易中心

--- src/components/menu/menu-three.vue
+++ src/components/menu/menu-three.vue
@@ -11,13 +11,13 @@
                 <div v-for="item in items">
                     <router-link :to="{ name: (item.text)}">
                         <el-row class="smallimg">
-                          <el-col :span="16" class="pd" >  
+                          <el-col :span="12" class="pd" >  
                                 <p class="menu-strategy ">{{item.text}}</p>
-                                <div class="menu-strategy-small">{{item.txt}}</div>
-                                <span class="menu-time">{{item.time}}</span>
+                                <div class="menu-strategy-small omit">{{item.txt}}</div>
+                                <span class="menuTime">{{item.time}}</span>
                             </el-col>
                             
-                        <el-col :span="8">  
+                        <el-col :span="8" offset="4">  
                             <img :src="item.iconUrl" >
                         
                          </el-col>
@@ -30,14 +30,17 @@
                             
             </el-row>
             <el-row>
-                  <el-col :span="8" :offset="15">  
-                           <div class="menu-buttron">
+                  <el-col :span="8" :offset="14">  
+                           <!-- <div class="menu-buttron">
                                <a href="#">
                                    <img src="../../../static/testImg/write@2x.png">
                                    <p>讲个故事</p>
                                 </a>
-                            </div>
-                        
+                            </div> -->
+                        <el-button type="warning" round class="menuBtn">
+                           <img src="../../../static/testImg/write@2x.png">
+                            讲个故事
+                        </el-button>
                          </el-col>
             </el-row>
          </el-col>
@@ -52,19 +55,19 @@ export default {
             items: [
                 {
                     text: "红山玉文化源远流长",
-                    txt: "红山文化的玉文化是红山文化的先祖们集体的智慧而集中反......",
+                    txt: "红山文化的玉文化是红山文化的先祖们集体的智慧而集中反",
                     iconUrl: "static/testImg/secondCompany.png",
                     time: "17分钟"
                 },
                 {
                     text: "红山玉文化源远流长",
-                    txt: "红山文化的玉文化是红山文化的先祖们集体的智慧而集中反......",
+                    txt: "红山文化的玉文化是红山文化的先祖们集体的智慧而集中反",
                     iconUrl: "static/testImg/secondCompany.png",
                     time: "17分钟"
                 },
                 {
                     text: "红山玉文化源远流长",
-                    txt: "红山文化的玉文化是红山文化的先祖们集体的智慧而集中反......",
+                    txt: "红山文化的玉文化是红山文化的先祖们集体的智慧而集中反",
                     iconUrl: "static/testImg/secondCompany.png",
                     time: "17分钟"
                 }
@@ -78,4 +81,18 @@ export default {
 @import "../../assets/index/style.less";
 @import "../../assets/header.less";
 @import "../../assets/menu/menu.less";
+.menuTime{
+    position: absolute;
+    bottom: 10px;
+    .fz(font-size, 24);
+    color: #ADAEAF;
+  }
+  .menuBtn{
+          border-radius: 30px;
+          background-color: #DAB62E;
+          .fz(font-size, 34);
+          margin-right: 20px;
+  } .menuBtn img{
+      width: 24px;
+  }
 </style>
