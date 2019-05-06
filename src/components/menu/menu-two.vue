6a8a044fdfb3cb1ab2c8e26eb5ee09ead80de8fa
songyinuo <815517598@qq.com>
2019-05-06 15:57:22 +0800

    首页收藏

--- src/components/menu/menu-two.vue
+++ src/components/menu/menu-two.vue
@@ -1,214 +1,209 @@
 <template lang="html">
 
-    <el-container>
-        <el-header class="tittle">
-            <div class="back" @click="$router.go(-1)">
-                <i class="el-icon-arrow-left"></i>
+  <el-container>
+    <el-header class="tittle">
+      <div class="back" @click="$router.go(-1)">
+        <i class="el-icon-arrow-left"></i>
+      </div>
+      旅游文化
+    </el-header>
+    <el-col :span="22" :offset="1">
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="攻略" name="first">
+          <el-row>
+            <div v-for="item in items">
+              <router-link :to="{ name:'旅游攻略',query: { id: item.id }}">
+                <el-row class="smallimg">
+                  <el-col :span="12" class="pd">
+                    <p class="menu-strategy ">{{item.text}}</p>
+                    <div class="menu-strategy-small omit">{{item.txt}}</div>
+                    <div class="menuTime">{{item.time}}</div>
+                  </el-col>
+
+                  <el-col :span="8" :offset="4">
+                    <img :src="item.iconUrl">
+
+                  </el-col>
+                </el-row>
+              </router-link>
             </div>
-            旅游文化
-        </el-header>
-        <el-col :span="22" :offset="1">
-            <el-tabs v-model="activeName" @tab-click="handleClick">
-                <el-tab-pane label="攻略" name="first">
-                    <el-row>
-                        <div v-for="item in items">
-                            <router-link :to="{ name:'旅游攻略',query: { id: item.id }}">
-                                <el-row class="smallimg">
-                                    <el-col :span="12" class="pd">
-                                        <p class="menu-strategy ">{{item.text}}</p>
-                                        <div class="menu-strategy-small omit">{{item.txt}}</div>
-                                        <div class="menuTime">{{item.time}}</div>
-                                    </el-col>
+          </el-row>
+        </el-tab-pane>
+        <el-tab-pane label="社区" name="second">
+          <div v-for="array in arrays">
+            <router-link :to="{name: '种草详情',query: { id: array.id }}">
+              <el-row>
+                <el-col :span="22">
+                  <img :src="array.iconUrl" class="fl menuHeader">
+                  <div class="fl menuHead-txt">{{array.text}}</div>
+                  <div class="fl cl menuHead-smtxt omit">{{array.txt}}</div>
+                  <p class="smalltxt-p cl fl">{{array.paragraph}}</p>
+                  <div class="cl smalltxt-div">{{array.article}}</div>
 
-                                    <el-col :span="8" :offset="4">
-                                        <img :src="item.iconUrl">
-
-                                    </el-col>
-                                </el-row>
-                            </router-link>
-                        </div>
+                </el-col>
+              </el-row>
+              <el-row type="flex" class="row-bg smallimg " justify="space-around">
 
 
-
-                    </el-row>
-                </el-tab-pane>
-                <el-tab-pane label="社区" name="second">
-                    <div v-for="array in arrays">
-                        <router-link :to="{name: '种草详情',query: { id: array.id }}">
-                            <el-row>
-                                <el-col :span="22">
-                                    <img :src="array.iconUrl" class="fl menuHeader">
-                                    <div class="fl menuHead-txt">{{array.text}}</div>
-                                    <div class="fl cl menuHead-smtxt omit">{{array.txt}}</div>
-                                    <p class="smalltxt-p cl fl">{{array.paragraph}}</p>
-                                    <div class="cl smalltxt-div">{{array.article}}</div>
-
-                                </el-col>
-                            </el-row>
-                            <el-row type="flex" class="row-bg smallimg " justify="space-around">
-
-
-                                <el-col :span="6" :offset="3"><img :src="array.photoUrl"></el-col>
-                                <el-col :span="6"><img :src="array.photoUrl"></el-col>
-                                <el-col :span="6"><img :src="array.photoUrl"></el-col>
+                <el-col :span="6" :offset="3"><img :src="array.photoUrl"></el-col>
+                <el-col :span="6"><img :src="array.photoUrl"></el-col>
+                <el-col :span="6"><img :src="array.photoUrl"></el-col>
 
 
 
-                            </el-row>
+              </el-row>
 
-                        </router-link>
-                    </div>
-                    <div v-for="avideo in avideos">
-                        <router-link :to="{name: '种草详情视频',query: { id: avideo.id }}">
-                            <el-row>
-                                <el-col :span="22">
-                                    <img :src="avideo.iconUrl" class="fl menuHeader">
-                                    <div class="fl menuHead-txt">{{avideo.text}}</div>
-                                    <div class="fl cl menuHead-smtxt">{{avideo.txt}}</div>
-                                    <p class="smalltxt-p cl fl">{{avideo.paragraph}}</p>
-                                    <div class="cl smalltxt-div">{{avideo.article}}</div>
+            </router-link>
+          </div>
+          <div v-for="avideo in avideos">
+            <router-link :to="{name: '种草详情视频',query: { id: avideo.id }}">
+              <el-row>
+                <el-col :span="22">
+                  <img :src="avideo.iconUrl" class="fl menuHeader">
+                  <div class="fl menuHead-txt">{{avideo.text}}</div>
+                  <div class="fl cl menuHead-smtxt">{{avideo.txt}}</div>
+                  <p class="smalltxt-p cl fl">{{avideo.paragraph}}</p>
+                  <div class="cl smalltxt-div">{{avideo.article}}</div>
 
-                                </el-col>
-                            </el-row>
-                            <el-row :gutter="22" class="mg ">
+                </el-col>
+              </el-row>
+              <el-row :gutter="22" class="mg ">
 
-                                <el-col :span="22" :offset="3" class=" smallimg">
-                                    <video :src="avideo.videoUrl" controls="controls" class="menu-video"></video>
-                                </el-col>
+                <el-col :span="22" :offset="3" class=" smallimg">
+                  <video :src="avideo.videoUrl" controls="controls" class="menu-video"></video>
+                </el-col>
 
-                            </el-row>
+              </el-row>
 
-                        </router-link>
+            </router-link>
 
-                    </div>
-                    <div v-for="array in arrays">
-                        <router-link :to="{name: '种草详情',query: { id: array.id }}">
-                            <el-row>
-                                <el-col :span="22">
-                                    <img :src="array.iconUrl" class="fl menuHeader">
-                                    <div class="fl menuHead-txt">{{array.text}}</div>
-                                    <div class="fl cl menuHead-smtxt">{{array.txt}}</div>
-                                    <p class="smalltxt-p cl fl">{{array.paragraph}}</p>
-                                    <div class="cl smalltxt-div">{{array.article}}</div>
+          </div>
+          <div v-for="array in arrays">
+            <router-link :to="{name: '种草详情',query: { id: array.id }}">
+              <el-row>
+                <el-col :span="22">
+                  <img :src="array.iconUrl" class="fl menuHeader">
+                  <div class="fl menuHead-txt">{{array.text}}</div>
+                  <div class="fl cl menuHead-smtxt">{{array.txt}}</div>
+                  <p class="smalltxt-p cl fl">{{array.paragraph}}</p>
+                  <div class="cl smalltxt-div">{{array.article}}</div>
 
-                                </el-col>
-                            </el-row>
-                            <el-row type="flex" class="row-bg smallimg " justify="space-around">
-
-
-                                <el-col :span="6" :offset="3"><img :src="array.photoUrl"></el-col>
-                                <el-col :span="6"></el-col>
-                                <el-col :span="6"></el-col>
+                </el-col>
+              </el-row>
+              <el-row type="flex" class="row-bg smallimg " justify="space-around">
 
 
+                <el-col :span="6" :offset="3"><img :src="array.photoUrl"></el-col>
+                <el-col :span="6"></el-col>
+                <el-col :span="6"></el-col>
 
-                            </el-row>
 
-                        </router-link>
-                    </div>
-                    <el-col>
-                        <router-link :to="{ name:'发布'}">
-                            <span class="flbtn"><img src="static/testImg/write@2x.png">发布</span>
-                        </router-link>
-                    </el-col>
-                    <el-row>
-                        <el-col :span="24">
-                            <div class="emptyFOOT"></div>
-                        </el-col>
-                    </el-row>
-                </el-tab-pane>
-            </el-tabs>
-        </el-col>
-    </el-container>
+
+              </el-row>
+
+            </router-link>
+          </div>
+          <el-col>
+            <router-link :to="{ name:'发布'}">
+              <span class="flbtn"><img src="static/testImg/write@2x.png">发布</span>
+            </router-link>
+          </el-col>
+          <el-row>
+            <el-col :span="24">
+              <div class="emptyFOOT"></div>
+            </el-col>
+          </el-row>
+        </el-tab-pane>
+      </el-tabs>
+    </el-col>
+  </el-container>
 </template>
 <script type="text/javascript">
-    export default {
-        data() {
-            return {
-                activeName: 'first',
-                items: [
-                    {
-                        text: "云南旅游攻略",
-                        txt: "云南是个风景秀丽的城市，值得推荐！......",
-                        iconUrl: "static/testImg/secondCompany.png",
-                        time: "17分钟",
-                        state: "审核中",
-                        id: "11000"
-                    },
-                    {
-                        text: "云南旅游攻略",
-                        txt: "云南是个风景秀丽的城市，值得推荐！......",
-                        iconUrl: "static/testImg/secondCompany.png",
-                        time: "17分钟",
-                        state: " ",
-                        id: "11001"
-                    },
-                    {
-                        text: "云南旅游攻略",
-                        txt: "云南是个风景秀丽的城市，值得推荐！......",
-                        iconUrl: "static/testImg/secondCompany.png",
-                        time: "17分钟",
-                        states: "已审核",
-                        id: "11003"
-                    }
-                ],
-                arrays: [
-                    {
-                        text: "云南旅游攻略",
-                        txt: "47分钟前",
-                        iconUrl: "static/testImg/shunPrincess.png",
-                        paragraph: "红山文化|论红山文化的历史性",
-                        article: "云南是个风景秀丽的城市，值得推荐！...",
-                        photoUrl: "static/testImg/secondCompany.png",
-                        id: "11004"
-                    },
-
-                ],
-                avideos: [
-                    {
-                        text: "云南旅游攻略",
-                        txt: "47分钟前",
-                        iconUrl: "static/testImg/shunPrincess.png",
-                        paragraph: "红山文化|论红山文化的历史性",
-                        article: "云南是个风景秀丽的城市，值得推荐！...",
-                        videoUrl: "http://www.runoob.com/try/demo_source/movie.mp4",
-                        id: "11005"
-                    },
-
-                ]
-            }
+export default {
+  data() {
+    return {
+      activeName: "first",
+      items: [
+        {
+          text: "云南旅游攻略",
+          txt: "云南是个风景秀丽的城市，值得推荐！......",
+          iconUrl: "static/testImg/secondCompany.png",
+          time: "17分钟",
+          state: "审核中",
+          id: "11000"
         },
-        methods: {
-            handleClick(tab, event) {
-                console.log(tab, event);
-            }
+        {
+          text: "云南旅游攻略",
+          txt: "云南是个风景秀丽的城市，值得推荐！......",
+          iconUrl: "static/testImg/secondCompany.png",
+          time: "17分钟",
+          state: " ",
+          id: "11001"
+        },
+        {
+          text: "云南旅游攻略",
+          txt: "云南是个风景秀丽的城市，值得推荐！......",
+          iconUrl: "static/testImg/secondCompany.png",
+          time: "17分钟",
+          states: "已审核",
+          id: "11003"
         }
+      ],
+      arrays: [
+        {
+          text: "云南旅游攻略",
+          txt: "47分钟前",
+          iconUrl: "static/testImg/shunPrincess.png",
+          paragraph: "红山文化|论红山文化的历史性",
+          article: "云南是个风景秀丽的城市，值得推荐！...",
+          photoUrl: "static/testImg/secondCompany.png",
+          id: "11004"
+        }
+      ],
+      avideos: [
+        {
+          text: "云南旅游攻略",
+          txt: "47分钟前",
+          iconUrl: "static/testImg/shunPrincess.png",
+          paragraph: "红山文化|论红山文化的历史性",
+          article: "云南是个风景秀丽的城市，值得推荐！...",
+          videoUrl: "http://www.runoob.com/try/demo_source/movie.mp4",
+          id: "11005"
+        }
+      ]
     };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    }
+  }
+};
 </script>
 <style lang="less">
-    @import "../../assets/index/style.less";
-    @import "../../assets/header.less";
-    @import "../../assets/menu/menu.less";
+@import "../../assets/index/style.less";
+@import "../../assets/header.less";
+@import "../../assets/menu/menu.less";
 
-    .menuTime {
-        position: absolute;
-        bottom: 10px;
-        .fz(font-size, 24);
-        color: #ADAEAF;
-    }
+.menuTime {
+  position: absolute;
+  bottom: 10px;
+  .fz(font-size, 24);
+  color: #adaeaf;
+}
 
-    .menuHead-smtxt {
-        margin-left: 45px;
-        .fz(font-size, 22);
-        color: #949494;
-        margin-top: -20px;
-        text-overflow: ellipsis;
-    }
+.menuHead-smtxt {
+  margin-left: 45px;
+  .fz(font-size, 22);
+  color: #949494;
+  margin-top: -20px;
+  text-overflow: ellipsis;
+}
 
-    .smalltxt-p {
-        .fz(font-size, 28);
-        color: #313131;
-        font-weight: bold;
-        margin-left: 45px;
-    }
+.smalltxt-p {
+  .fz(font-size, 28);
+  color: #313131;
+  font-weight: bold;
+  margin-left: 45px;
+}
 </style>
---
+++ src/views/Collect.vue
@@ -0,0 +1,372 @@
+<template lang="html">
+  <div class="index">
+    <el-header class="tittle">
+      <div class="back" @click="$router.go(-1)">
+        <i class="el-icon-arrow-left"></i>
+      </div>
+      收藏
+    </el-header>
+    <el-main class="collectInfo">
+      <el-row>
+        <el-col :span="22" :offset="1">
+          <el-tabs v-model="collect.activeName" @tab-click="handleClick">
+             <el-tab-pane :label="k.label" :name="k.name" v-for="k in collect.collectionClassified" v-if="k.label==='商品'">
+                 <el-col :span="22" :offset="1" class="pd2 brB1" v-for="o in k.infoArray">
+                  <el-col :span="6" class="collectInfoProductDrawing">
+                    <router-link :to="{ name: '详情页',query: { id: o.id, title: k.label } }">
+                      <img :src="o.imgUrl">
+                    </router-link>
+                  </el-col>
+                  <el-col :span="17" :offset="1">
+                    <div class="mgT4">{{o.title}}</div>
+                    <div class="pd2 colorRed">￥{{o.price}}</div>
+                  </el-col>
+                 </el-col>
+             </el-tab-pane>
+              <el-tab-pane :label="k.label" :name="k.name" v-for="k in collect.collectionClassified" v-if="k.label==='种草'">
+                 <el-col :span="22" :offset="1" class="pd2 brB1" v-for="o in k.infoArray">
+                    <el-col :span="4" class="uUrlImg">
+                      <img :src="o.uUrlImg">
+                    </el-col>
+                    <router-link :to="{ name: '种草详情',query: { id: o.id } }" v-if="o.imgUrl!==''">
+                      <el-col :span="19" :offset="1">
+                        <div class="mgT2 colorGray">{{o.uName}}</div>
+                        <div class="pd1 colorGray uTime">{{o.uTime}}</div>
+                        <div class="pd2">{{o.title}}</div>
+                      </el-col>
+                    </router-link>
+                    <router-link :to="{ name: '种草详情视频',query: { id: o.id } }" v-if="o.videoUrl!==''">
+                      <el-col :span="19" :offset="1">
+                        <div class="mgT2 colorGray">{{o.uName}}</div>
+                        <div class="pd1 colorGray uTime">{{o.uTime}}</div>
+                        <div class="pd2">{{o.title}}</div>
+                      </el-col>
+                    </router-link>
+                    <el-col class="annotation">{{o.annotation}}</el-col>
+                    <el-col class="listImg pd1" v-if="o.imgUrl!==''"><img :src="o.imgUrl"></el-col>
+                    <el-col v-if="o.videoUrl!==''">
+                      <video :src="o.videoUrl" controls="controls" class="videoUrl"></video>
+                    </el-col>
+                 </el-col>
+             </el-tab-pane>
+             <el-tab-pane :label="k.label" :name="k.name" v-for="k in collect.collectionClassified" v-if="k.label==='攻略'|| k.label==='讲堂'">
+                 <el-col :span="22" :offset="1" class="pd2 brB1" v-for="o in k.infoArray">
+                  <el-col :span="11" class="collectInfoProductDrawing">
+                    <router-link :to="{ name: '种草详情',query: { id: o.id, title: k.label } }">
+                      <img :src="o.imgUrl">
+                    </router-link>
+                  </el-col>
+                  <el-col :span="12" :offset="1">
+                    <div class="mgT10">{{o.title}}</div>
+                    <div class="pd2 colorRed">￥{{o.price}}</div>
+                  </el-col>
+                 </el-col>
+             </el-tab-pane>
+            <el-tab-pane :label="k.label" :name="k.name" v-for="k in collect.collectionClassified" v-if="k.label==='书画'">
+                  <el-col :span="12" class="collectInfoProductDrawing pdTRBL2" v-for="o in k.infoArray">
+                    <router-link :to="{ name: '详情页',query: { id: o.id, title: k.label } }">
+                      <img :src="o.imgUrl">
+                    </router-link>
+                    <div class="mgT4 overHidden">{{o.title}}</div>
+                    <div class="pd2 colorRed overHidden">￥{{o.price}}</div>
+                  </el-col>
+             </el-tab-pane>
+          </el-tabs>
+        </el-col>
+      </el-row>
+    </el-main>
+    <v-footer />
+  </div>
+</template>
+
+<script>
+import Footer from "@/common/_footer.vue";
+export default {
+  components: {
+    "v-footer": Footer
+  },
+  data() {
+    return {
+      collect: {
+        activeName: "shangpin",
+        collectionClassified: [
+          {
+            label: "商品",
+            name: "shangpin",
+            infoArray: [
+              {
+                id: "idA10000001",
+                imgUrl: "static/testImg/vegetables-1.jpg",
+                title: "精选绿色蔬菜 纯绿色 无公害",
+                price: 152
+              },
+              {
+                id: "idA100000012",
+                imgUrl: "static/testImg/vegetables-1.jpg",
+                title: "1精选绿色蔬菜 纯绿色 无公害",
+                price: 153
+              },
+              {
+                id: "idA100000013",
+                imgUrl: "static/testImg/vegetables-1.jpg",
+                title: "2精选绿色蔬菜 纯绿色 无公害",
+                price: 154
+              },
+              {
+                id: "idA100000014",
+                imgUrl: "static/testImg/vegetables-1.jpg",
+                title: "3精选绿色蔬菜 纯绿色 无公害",
+                price: 155
+              },
+              {
+                id: "idA100000015",
+                imgUrl: "static/testImg/vegetables-1.jpg",
+                title: "4精选绿色蔬菜 纯绿色 无公害",
+                price: 156
+              },
+              {
+                id: "idA100000016",
+                imgUrl: "static/testImg/vegetables-1.jpg",
+                title: "5精选绿色蔬菜 纯绿色 无公害",
+                price: 157
+              }
+            ]
+          },
+          {
+            label: "攻略",
+            name: "gonglue",
+            infoArray: [
+              {
+                id: "idA20000001",
+                imgUrl: "static/testImg/pig01.jpg",
+                title: "小猪佩奇",
+                price: 152
+              },
+              {
+                id: "idA200000012",
+                imgUrl: "static/testImg/pig01.jpg",
+                title: "1小猪佩奇",
+                price: 153
+              },
+              {
+                id: "idA200000013",
+                imgUrl: "static/testImg/pig01.jpg",
+                title: "2小猪佩奇",
+                price: 154
+              },
+              {
+                id: "idA200000014",
+                imgUrl: "static/testImg/pig01.jpg",
+                title: "3小猪佩奇",
+                price: 155
+              },
+              {
+                id: "idA200000015",
+                imgUrl: "static/testImg/pig01.jpg",
+                title: "4小猪佩奇",
+                price: 156
+              },
+              {
+                id: "idA200000016",
+                imgUrl: "static/testImg/pig01.jpg",
+                title: "5小猪佩奇",
+                price: 157
+              }
+            ]
+          },
+          {
+            label: "讲堂",
+            name: "jiangtang",
+            infoArray: [
+              {
+                id: "idA30000001",
+                imgUrl: "static/testImg/oringe-2.jpg",
+                title: "美味鲜橙",
+                price: 152
+              },
+              {
+                id: "idA300000012",
+                imgUrl: "static/testImg/oringe-2.jpg",
+                title: "1美味鲜橙",
+                price: 253
+              },
+              {
+                id: "idA300000013",
+                imgUrl: "static/testImg/oringe-2.jpg",
+                title: "2美味鲜橙",
+                price: 254
+              },
+              {
+                id: "idA300000014",
+                imgUrl: "static/testImg/oringe-2.jpg",
+                title: "3美味鲜橙",
+                price: 255
+              },
+              {
+                id: "idA300000015",
+                imgUrl: "static/testImg/oringe-2.jpg",
+                title: "4美味鲜橙",
+                price: 256
+              },
+              {
+                id: "idA300000016",
+                imgUrl: "static/testImg/oringe-2.jpg",
+                title: "5美味鲜橙",
+                price: 257
+              }
+            ]
+          },
+          {
+            label: "书画",
+            name: "shuhua",
+            infoArray: [
+              {
+                id: "idA30000001",
+                imgUrl: "static/testImg/youhua.jpg",
+                title: "经典书画",
+                price: 105002
+              },
+              {
+                id: "idA300000012",
+                imgUrl: "static/testImg/youhua.jpg",
+                title: "1经典书画 实力大师创作",
+                price: 22053
+              },
+              {
+                id: "idA300000013",
+                imgUrl: "static/testImg/youhua.jpg",
+                title: "2经典书画 实力大师创作",
+                price: 25400
+              },
+              {
+                id: "idA300000014",
+                imgUrl: "static/testImg/youhua.jpg",
+                title: "3经典书画 实力大师创作",
+                price: 25500
+              },
+              {
+                id: "idA300000015",
+                imgUrl: "static/testImg/youhua.jpg",
+                title: "4经典书画 实力大师创作",
+                price: 25600
+              },
+              {
+                id: "idA300000016",
+                imgUrl: "static/testImg/youhua.jpg",
+                title: "5经典书画 实力大师创作",
+                price: 25700
+              }
+            ]
+          },
+          {
+            label: "种草",
+            name: "zhongcao",
+            infoArray: [
+              {
+                id: "idA30000001",
+                uId: "Uid1000001",
+                uName: "柒个我",
+                uUrlImg: "static/testImg/Focus2.png",
+                uTime: "5分钟",
+                imgUrl: "static/testImg/tourism01.jpg",
+                videoUrl: "",
+                title: "今天天气真好",
+                annotation:
+                  "春天天气真好花儿都开了杨柳树枝对着我们弯弯腰蝴蝶姑娘飞来了蜜蜂嗡嗡叫小白兔儿一跳一跳又一跳春天天气真好花儿都开了杨柳树枝对着我们弯弯腰"
+              },
+              {
+                id: "idA30000001",
+                uId: "Uid1000001",
+                uName: "柒个他",
+                uTime: "15分钟",
+                uUrlImg: "static/testImg/Focus2.png",
+                imgUrl: "static/testImg/youhua.jpg",
+                videoUrl: "",
+                title: "1今天天气真好",
+                annotation:
+                  "春天天气真好花儿都开了杨柳树枝对着我们弯弯腰蝴蝶姑娘飞来了蜜蜂嗡嗡叫小白兔儿一跳一跳又一跳春天天气真好花儿都开了杨柳树枝对着我们弯弯腰"
+              },
+              {
+                id: "idA30000001",
+                uId: "Uid1000001",
+                uName: "柒个你",
+                uTime: "45分钟",
+                uUrlImg: "static/testImg/Focus2.png",
+                imgUrl: "",
+                videoUrl: "http://www.runoob.com/try/demo_source/movie.mp4",
+                title: "2今天天气真好",
+                annotation:
+                  "春天天气真好花儿都开了杨柳树枝对着我们弯弯腰蝴蝶姑娘飞来了蜜蜂嗡嗡叫小白兔儿一跳一跳又一跳春天天气真好花儿都开了杨柳树枝对着我们弯弯腰"
+              },
+              {
+                id: "idA30000001",
+                uId: "Uid1000001",
+                uName: "柒个她",
+                uTime: "昨天",
+                uUrlImg: "static/testImg/Focus2.png",
+                imgUrl: "static/testImg/tourism02.jpg",
+                videoUrl: "",
+                title: "3今天天气真好",
+                annotation:
+                  "春天天气真好花儿都开了杨柳树枝对着我们弯弯腰蝴蝶姑娘飞来了蜜蜂嗡嗡叫小白兔儿一跳一跳又一跳春天天气真好花儿都开了杨柳树枝对着我们弯弯腰"
+              }
+            ]
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      // console.log(tab, event);
+    }
+  }
+};
+</script>
+
+<style lang="less">
+@import "../assets/header.less";
+@import "../assets/index/style.less";
+@import "../assets/search/search.less";
+.el-main {
+  padding: 0;
+}
+.collectInfo {
+  .el-tabs__active-bar {
+    display: none;
+  }
+  .el-tabs__nav-wrap::after {
+    display: none;
+  }
+  .el-tabs--top .el-tabs__item.is-top:nth-child(2) {
+    padding: 0 20px;
+  }
+  padding-bottom: 48px;
+  .collectInfoProductDrawing {
+    img {
+      width: 100%;
+    }
+  }
+  .uUrlImg {
+    img {
+      width: 48px;
+      height: 48px;
+    }
+  }
+  .uTime {
+    .fz(font-size, 24);
+  }
+  .annotation {
+    .fz(font-size, 28);
+    line-height: 1.4;
+  }
+  .listImg {
+    img {
+      width: 100%;
+    }
+  }
+  .videoUrl {
+    width: 100%;
+  }
+}
+</style>
