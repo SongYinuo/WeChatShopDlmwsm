d51f0fcc63a5783a17ea3f4fa9313a8190e584be
songyinuo <815517598@qq.com>
2019-05-18 13:44:26 +0800

    更改发布鉴定上传页，post接口无法连接等后台上班调试

--- src/views/login.vue
+++ src/views/login.vue
@@ -42,20 +42,20 @@
 </template>
 
 <script>
-import Header from '@/common/_header.vue'
+import Header from "@/common/_header.vue";
 import { Toast } from 'mint-ui'
 export default {
-  components:{
-    'v-header':Header
+  components: {
+    "v-header": Header
   },
-  data(){
+  data() {
     return {
       account:'',
       password:'',
       toggle:!this.$store.state.login.token
-    }
+    };
   },
-  methods:{
+  methods: {
     // 登录按钮
     login(){
       if(this.account!=="" && this.password!=="") {
@@ -74,7 +74,7 @@ export default {
       // 登录成
     },
 
-    //退出登录按钮
+    // 退出登录按钮
     logout(){
       Toast('退出登录成功,清除token');
       this.$store.commit('CHANGE_TOKEN',0);
@@ -82,19 +82,56 @@ export default {
       this.account = '';
       this.password = '';
 
-    }
-  }
-}
+    },
+    // getCode() {
+    //   // 非静默授权，第一次有弹框
+    //   this.code = "";
+    //   var local = window.location.href; // 获取页面url
+    //   var appid = "wx65adcf0753691319";
+    //   this.code = this.getUrlCode().code; // 截取code
+    //   if (this.code == null || this.code === "") {
+    //     // 如果没有code，则去请求
+    //     window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${encodeURIComponent(
+    //       local
+    //     )}&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect`;
+    //   } else {
+    //     // 你自己的业务逻辑
+    //   }
+    // },
+    // getUrlCode() {
+    //   // 截取url中的code方法
+    //   var url = location.search;
+    //   this.winUrl = url;
+    //   var theRequest = new Object();
+    //   if (url.indexOf("?") != -1) {
+    //     var str = url.substr(1);
+    //     var strs = str.split("&");
+    //     for (var i = 0; i < strs.length; i++) {
+    //       theRequest[strs[i].split("=")[0]] = strs[i].split("=")[1];
+    //     }
+    //   }
+    //   return theRequest;
+    // }
+  },
 
+  // mounted() {
+  //   if (!window.localStorage.getItem("openId")) {
+  //     // 如果缓存localStorage中没有微信openId，则需用code去后台获取
+  //     this.getCode();
+  //   } else {
+  //     // 别的业务逻辑
+  //   }
+  // }
+};
 </script>
 
 <style lang="less" scoped>
 @import "../assets/header.less";
 .login {
-  >section {
+  > section {
     .tip {
       padding: 6vw 3vw;
-      color:rgb(224, 145, 71);
+      color: rgb(224, 145, 71);
       letter-spacing: 2px;
       font-size: 16px;
     }
