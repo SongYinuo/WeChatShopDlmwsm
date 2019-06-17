22e76440bd14c3042384e6e72c99ac2aff3a6844
songyinuo <815517598@qq.com>
2019-06-17 10:33:28 +0800

    格式化页面

--- src/components/index/integralShop.vue
+++ src/components/index/integralShop.vue
@@ -8,7 +8,7 @@
             </router-link>
             </div>
           </el-col>
-         <el-col :span="22" :offset="1" class="list" >
+         <el-col :span="22" :offset="1" class="list brB1">
          <el-col :span="8"  v-for="swiper in swiperList.goods_list" class="listText">
           <router-link :to="{ name: '详情页',params:{id:swiper.goods_id} }">
             <img :src="swiper.original_img" >
