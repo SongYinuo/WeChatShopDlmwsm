7c6a37ec0dc2e14b05de361ad0bf277204ecefa9
drxiiiiiii <1228685424@qq.com>
2019-06-18 17:06:24 +0800

    huye-支付测试

--- src/components/user/userVip.vue
+++ src/components/user/userVip.vue
@@ -17,7 +17,7 @@
               <div class="text-alignCenter colorWhite userVipTitle pdT3">我办理的Vip</div>
               <div class="text-alignCenter colorWhite userVipDay pdT1">还有{{userVip.remainingDays}}天到期</div>
               <div class="text-alignCenter userVipTitle pdT3">
-                <el-button plain>立即续费</el-button>
+                <el-button plain @click="wxpay">立即续费</el-button>
               </div>
             </el-col>
           </el-row>
@@ -32,6 +32,7 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
@@ -41,6 +42,59 @@ export default {
         userVipExplain: "此处为有关vip的说明文字"
       }
     };
+  },
+  methods: {
+    wxpay() {
+      axios({
+        methods: "post",
+        async: "false",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        url: "/Api/Payment/vip_pay"
+      })
+        // .post(url, data)
+        .then(res => {
+          if (res.code == 200) {
+            const callpay = res.data.jsApiParameters;
+            if (typeof WeixinJSBridge == "undefined") {
+              if (document.addEventListener) {
+                document.addEventListener(
+                  "WeixinJSBridgeReady",
+                  onBridgeReady,
+                  false
+                );
+              } else if (document.attachEvent) {
+                document.attachEvent("WeixinJSBridgeReady", onBridgeReady);
+                document.attachEvent("onWeixinJSBridgeReady", onBridgeReady);
+              }
+            } else {
+              onBridgeReady();
+              // this.onBridgeReady(pay_params);
+            }
+          } else {
+            alert("微信支付调起失败！");
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    onBridgeReady(params) {
+      const pay_params = JSON.parse(params);
+      WeixinJSBridge.invoke("getBrandWCPayRequest", jsApiParameters, function(
+        res
+      ) {
+        //WeixinJSBridge.log(res.err_msg);
+        if (res.err_msg == "get_brand_wcpay_request:ok") {
+          // location.href='$go_url';
+        } else {
+          alert(res.err_code + res.err_desc + res.err_msg+"1");
+          // location.href='$back_url';
+         
+        }
+      });
+    }
   }
 };
 </script>
