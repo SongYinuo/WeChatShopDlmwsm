8801930427f73eaf7b08d27d7ca44ade0a6d181a
drxiiiiiii <1228685424@qq.com>
2019-06-20 15:26:55 +0800

    huye-VIP支付第四版

--- src/components/user/userVip.vue
+++ src/components/user/userVip.vue
@@ -41,38 +41,116 @@ export default {
         remainingDays: 40,
         userVipExplain: "此处为有关vip的说明文字"
       },
-      api:{}
-
+      api: {}
     };
   },
   methods: {
-    wxpay() {
-    const that= this;
-    axios
-      .get("/Api/Payment/vip_pay")
-      // .then(function(res){
-      //   that.api=JSON.parse(res.data.data);
-      //   console.log(1);
-      //    console.log(res);
-      //    console.log(666);
-      //    console.log(that.api);
-
-      //   // alert(res.data.data);
-      // })
-      .then(res => {
-         console.log(1);
-          console.log(res);
-          console.log(666);
-          console.log(res.data.data);
-          that.api = JSON.parse(res.data.data);
-          console.log(888);
-          console.log(that.api);
-        })
-        .catch(error => {});
-    },
-   
-  }
-};
+  //   wxpay() {
+  //     const that = this;
+  //     axios.post("/Api/Payment/vip_pay")
+  //     // .then(res => {
+  //     //     that.api = JSON.parse(res.data.data);
+  //     //   })
+  //     //   .catch(error => {});
+  //    .then((res) => {
+  //               if(res.code == 200) {
+  //                   const api = JSON.parse(res.data.data);
+  //                   console.log(1);
+  //                   console.log(res.data.data);
+  //                   console.log(2);
+  //                   console.log(api);
+  //                   if (typeof WeixinJSBridge == "undefined"){
+  //                       if( document.addEventListener ){
+  //                           document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+  //                       }else if (document.attachEvent){
+  //                           document.attachEvent('WeixinJSBridgeReady', onBridgeReady); 
+  //                           document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+  //                       }
+  //                       }else{
+  //                       this.onBridgeReady(pay_params);
+  //                   }
+  //               }else{
+  //                   alert('微信支付调起失败！');
+  //               }
+  //           }).catch((err) => {
+  //               console.log(err);
+  //           })
+  //       },
+  // },}
+  //        onBridgeReady(params) {
+  //           const api = JSON.parse(res.data.data);
+  //           console.log(3);
+  //           console.log(res.data.data);
+  //           console.log(4);
+  //           console.log(api);
+  //           WeixinJSBridge.invoke(
+  //               'getBrandWCPayRequest', {
+  //                   "appId": api.appId,  //公众号名称，由商户传入     
+  //                   "timeStamp":api.timeStamp,  //时间戳，自1970年以来的秒数     
+  //                   "nonceStr":api.nonceStr,  //随机串     
+  //                   "package": api.package,     
+  //                   "signType": api.signType,  //微信签名方式：     
+  //                   "paySign": api.paySign  //微信签名 
+  //               },
+  //               function(res){
+  //                   if(res.err_msg == "get_brand_wcpay_request:ok" ){
+  //                   alert('支付成功！');
+  //               } 
+  //           }); 
+  //       }
+   wxpay() {
+           axios.post("/Api/Payment/vip_pay")
+            .then((res) => {
+                if(res.code == 1) {
+                     const api = JSON.parse(res.data.data);
+                    console.log(1);
+                    console.log(res.data.data);
+                    console.log(2);
+                    console.log(api);
+                    if (typeof WeixinJSBridge == "undefined"){
+                        if( document.addEventListener ){
+                            document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+                        }else if (document.attachEvent){
+                            document.attachEvent('WeixinJSBridgeReady', onBridgeReady); 
+                            document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+                        }
+                        }else{
+                        this.onBridgeReady(pay_params);
+                    }
+                }else{
+                    alert('微信支付调起失败！');
+                }
+            }).catch((err) => {
+                console.log(err);
+            })
+        },
+        onBridgeReady(params) {
+            const pay_params = JSON.parse(api);
+            console.log(3);
+            console.log(res.data.data);
+            console.log(4);
+            console.log(api);
+            console.log(5);
+            console.log(pay_params);
+            WeixinJSBridge.invoke(
+                'getBrandWCPayRequest', {
+                    "appId": api.appId,  //公众号名称，由商户传入     
+                    "timeStamp":api.timeStamp,  //时间戳，自1970年以来的秒数     
+                    "nonceStr":api.nonceStr,  //随机串     
+                    "package": api.package,     
+                    "signType": api.signType,  //微信签名方式：     
+                    "paySign": api.paySign  //微信签名 
+                },
+                function(res){
+                    if(res.err_msg == "get_brand_wcpay_request:ok" ){
+                    alert('支付成功！');
+                } 
+            }); 
+        }
+        }
+        }
+  
+      
 </script>
 
 <style lang="less">
